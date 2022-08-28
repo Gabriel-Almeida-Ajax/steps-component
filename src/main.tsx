6faaa3f82fc6010f92c70cc012662018bf8f939d
Gabriel Santos Lima <lima.gabriel@lello.com.br>
2022-08-28 04:08:52 -0300

    ajust

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { Theme } from "./components/Theme";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
-  </React.StrictMode>
-);
+export { StepComponent } from "./view/index";
+export { StepProvider, useStep } from "./hook";
--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,18 +1,19 @@
 import { Paper } from "../components/Paper/styles";
 import { Stepper, StepperConfigProps } from "../components/Stepper/Stepper";
+import { Theme } from "../components/Theme";
 import { Header } from "../components/Title/Title";
 import { useStep } from "../hook";
 
 export const StepComponent = ({ ...config }: StepperConfigProps) => {
   const { step, steps } = useStep();
   return (
-    <>
+    <Theme>
       {steps.length && (
         <Paper>
           <Header subtitle={steps[step].title} />
           <Stepper {...{ step, steps, ...config }} />
         </Paper>
       )}
-    </>
+    </Theme>
   );
 };
